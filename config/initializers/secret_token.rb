c035d601d1a9b7966c39c99775465f62a7349c89
Julia Rhi <juliarhi@gmail.com>
2012-11-04 15:45:26 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DinnerTyme::Application.config.secret_token = 'b40c2ecb93313cc140125191053a575784a4d1c32268363b7f13c5c89e723cf8a97cf0b4de6cfb2ffeafe57bd0888e780a02dfe54d831d00df747b8c213a5b7e'
